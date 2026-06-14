b24be43b5661628eba3c9a514bbcba0674949211
Владислав Дружицький <druzhytskyivlad@gmail.com>
2026-06-15 01:19:10 +0300

    new file:   mysite/polls/static/polls/style.css

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,25 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """was_published_recently() повертає False для питань, pub_date яких у майбутньому."""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+def create_question(question_text, days):
+    """Створює питання із заданим текстом та зміщенням дат."""
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """Якщо питань немає, виводиться відповідне повідомлення."""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
